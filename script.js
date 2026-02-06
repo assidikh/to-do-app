de16a21fe8068c1231a1e70a8068bb45eb183ec9
Aboubakar Assidikh <aboubakarassidikh@gmail.com>
2026-02-06 08:19:17 +0100

    feat: Validation de l'input et ajout d'une nouvelle tâche

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Créer le tableau de tâches
-let task = []
+let tasks = []
 
 //Fonction pour créer une nouvelle tâche
 function createTask(taskText){
@@ -10,9 +10,9 @@ function createTask(taskText){
 }
 
 //Tester la fonction
-task.push(createTask("My first task")) //insérer une tâche dans le tableau task
-task.push(createTask("My second task")) //insérer une deuxième tâche dans le tableau task
-console.log(task) //Afficher mon tableau task dans la console
+tasks.push(createTask("My first task")) //insérer une tâche dans le tableau tasks
+tasks.push(createTask("My second task")) //insérer une deuxième tâche dans le tableau tasks
+console.log(tasks) //Afficher mon tableau tasks dans la console
 
 
 //Récupération des éléments html
@@ -23,6 +23,21 @@ const taskInput = document.getElementById("taskInput")
 taskForm.addEventListener("submit", (event) => {
     event.preventDefault()
 
-    const taskText = taskInput.value
-    console.log(taskText)
+    const taskText = taskInput.value.trim() //Récupération du texte saisie dans le champ taskInput
+
+    //Validation
+    if(taskText === ""){
+        console.log("Erreur: Le champs est vide !")
+        return
+    }
+
+    //Création et ajout de la tâche
+    const newTask = createTask(taskText)
+    tasks.push(newTask)
+
+    console.log("Tâches: ", tasks)
+
+    //Réinitialisatin du champ
+    taskInput.value = ""
+
 })
